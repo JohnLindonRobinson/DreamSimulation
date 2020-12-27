85e99d203c2076ead09f9e6c5df88b37f9c7ca0b
JohnLindonRobinson <john.lindon.robinson@gmail.com>
2020-12-27 04:59:49 +0000

    After researching how many trades it took dream each run, I realised it was more complex than I realised. I decided to commit my work and go to bed for the night

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Test")
+import random
+import matplotlib
+
+def pearl_trade():
+	return random.uniform(0, 1)<20/423
+
+def sufficient_pearl(aim=10):
+	number_of_pearls = 0
+	number_of_attempts = 0
+	while number_of_pearls<aim:
+		if pearl_trade():
+			number_of_pearls+=1
+		number_of_attempts+=1
+	return number_of_attempts, number_of_attempts<10.5
+
+print(sufficient_pearl())
+
+#After researching how many trades it took dream each run, I realised it was more complex than I realised. I decided to commit my work and go to bed for the night
